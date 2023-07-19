61e8aafc1c7c31cd66ceac57e9fb5b44295e901d
charbonierlouis <louis.charbonier@gmail.com>
2023-07-19 11:43:35 +0200

    chore(assets): create assets config

---
+++ src/config/assets.ts
@@ -0,0 +1,20 @@
+import { Assets } from '@/types/assets';
+
+export const assets: Assets = [{
+  id: '1-mysterious-warriors',
+  path: '/assets/1-mysterious-warriors.png',
+  name: 'Mysterious Warriors',
+}, {
+  id: '2-elephant-pink',
+  path: '/assets/2-elephant-pink.png',
+  name: 'Elephant Pink',
+}, {
+  id: '3-icescream',
+  path: '/assets/3-icescream.png',
+  name: 'Icescream',
+}, {
+  id: '4-fox',
+  path: '/assets/4-fox.png',
+  name: 'Fox',
+},
+];
