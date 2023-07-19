ed96c4de85235affd5565a088898308220d4f719
charbonierlouis <louis.charbonier@gmail.com>
2023-07-19 16:37:32 +0200

    feat(header): add aria-label to home button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -13,7 +13,11 @@ export default function Header({
     <header>
       <div className=" sticky top-0 w-full bg-base-200 p-5 flex flex-col md:flex-row items-center justify-between">
         <div className="flex gap-2 items-center">
-          <Link href="/" className="btn btn-ghost">
+          <Link
+            href="/"
+            className="btn btn-ghost"
+            aria-label="Accueil"
+          >
             <HomeIcon className="w-6 h-6" />
           </Link>
           <h1 className="text-xl font-bold">{title}</h1>
