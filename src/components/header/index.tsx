09046a0127f4bbfed27e0c67476e727f63794063
charbonierlouis <louis.charbonier@gmail.com>
2023-07-19 16:01:00 +0200

    feat(header): add go to home button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,5 @@
+import { HomeIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 import SelectAsset from '../assets/SelectAsset';
 
 interface Props {
@@ -10,7 +12,12 @@ export default function Header({
   return (
     <header>
       <div className=" sticky top-0 w-full bg-base-200 p-5 flex flex-col md:flex-row items-center justify-between">
-        <h1 className="text-xl font-bold">{title}</h1>
+        <div className="flex gap-2 items-center">
+          <Link href="/" className="btn btn-ghost">
+            <HomeIcon className="w-6 h-6" />
+          </Link>
+          <h1 className="text-xl font-bold">{title}</h1>
+        </div>
         <SelectAsset />
       </div>
     </header>
