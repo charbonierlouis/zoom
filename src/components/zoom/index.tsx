e0e73b307c6d19b245dce186b695c35ee4fa966c
charbonierlouis <louis.charbonier@gmail.com>
2023-07-19 15:35:03 +0200

    feat(zoom): create zoom component

---
+++ src/components/zoom/index.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useZoom } from '@/hook/useZoom';
+import { AssetInfo } from '@/types/assets';
+import Image from 'next/image';
+
+interface Props {
+  assetInfo: AssetInfo;
+}
+
+export default function Zoom({ assetInfo }: Props) {
+  const {
+    containerRef,
+    cursorRef,
+    imageRef,
+  } = useZoom();
+
+  return (
+    <div
+      ref={containerRef}
+      className="imagecontainer"
+    >
+      <div
+        ref={cursorRef}
+        className="hidden cursor-zoom"
+        style={{
+          backgroundImage: `url(${assetInfo.path})`,
+        }}
+      />
+      <Image
+        src={assetInfo.path}
+        alt={assetInfo.name}
+        width={300}
+        height={300}
+        ref={imageRef}
+      />
+    </div>
+  );
+}
